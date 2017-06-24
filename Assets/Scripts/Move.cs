e44c99d93eade48513f572e42f0e20841dc809a8
Otavio Augusto <tavioalves@gmail.com>
2017-06-24 13:00:56 -0300

    Versão atual projeto

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+	public float Speed = 30f;
+	protected Vector3 movement;
+
+	void Update ()
+	{
+		movement = Vector3.left * (Speed / 10) * Speed * Time.fixedDeltaTime;
+		transform.Translate(movement,Space.World);	
+	}
+}
---
+++ Assets/Scripts/PointsManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour {
+
+	public Text textPoints;
+	int points;
+
+	void Start () {
+		textPoints.text = "POINTS: ";
+		points = 0;
+	}
+
+	void Update () {
+		textPoints.text = "POINTS: " + points;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.name == "Cube(Clone)")
+			points += 10;
+	}
+}
